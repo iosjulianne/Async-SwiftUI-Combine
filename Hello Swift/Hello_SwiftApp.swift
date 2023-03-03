92ca56d8b9d6d0153e73f30b44c712833e348852
iosjulianne <iosjulianne>
2023-03-03 09:51:09 -0500

    Initial Commit

---
+++ Hello Swift/Hello_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftApp.swift
+//  Hello Swift
+//
+//  Created by Julianne on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
