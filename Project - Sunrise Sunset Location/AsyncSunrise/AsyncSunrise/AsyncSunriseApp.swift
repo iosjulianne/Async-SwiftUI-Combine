c66e10409f445d90e09636848490eb84b9f2f238
Julianne <58789769+iosjulianne@users.noreply.github.com>
2023-07-24 21:56:06 -0400

    Create xcode proj

---
+++ Project - Sunrise Sunset Location/AsyncSunrise/AsyncSunrise/AsyncSunriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSunriseApp.swift
+//  AsyncSunrise
+//
+//  Created by Julianne on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSunriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
